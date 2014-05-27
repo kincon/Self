9e8d7d07cf72aa6519cf8973d5f96dd3e76d948a
kincon <chessconan@gmail>
2014-05-27 10:41:43 -0700

    Update SpeakeasyTest.py

--- SpeakeasyTest.py
+++ SpeakeasyTest.py
@@ -45,21 +45,28 @@ class Human(object):
             self.bar = bar
             while bar == True:
                 for x in stock:
-                    '''
+                    """
                     print x
                     print "Drink price: " + str(price[x])
                     print "Customer's money: " + str(money)
                     print "Thirst: " + str(thirst)
-                   '''
+                    """
                     if price[x] <= thirst and money >= price[x]:
                         self.money -= price[x]
                         stock[x] -= 1
                         self.thirst -= price[x]
-                        print "%s bought a %s! They have %s dollars left to sate their thirst. Thirst: %s" % (self.name, x, str(self.money), self.thirst)
+                        print "The Customer bought a %s! They have %s dollars left to sate their thirst. Thirst: %s" % (x, str(self.money), self.thirst)
+                        return
+                        break
+                    
+                    """
                     elif price[x] >= money and thirst >= price[x]:
                         desparation += price[x] / 2
                         return
-                    else:
+                        """
+                    
+                    #Why does this else statement create an error if it's tabbed in one more time? Ask Paul.
+                else:
                         print "what have you done"
                         return
                         break
@@ -79,7 +86,7 @@ my_bar_prices = {
 
 my_bar_stock = {
     "Hooch": 20,
-    "Booze": 19,
+    "Booze": 10,
     "Whiskey": 5,
     "Scotch": 3,
     "The Good Stuff": 1
@@ -87,19 +94,47 @@ my_bar_stock = {
     
 Jones = Human("Jones", 1000, 0, 0, True, False, 100, 0)
 Charles = Human("Charles", 100, 10, 0, False, True, 5, 100)
+
 EmployeeList = [Jones.name]
 CustomerList = [Charles.name]
 HumanList = [EmployeeList[::], CustomerList[::]]
 
+"""
 mySpeakeasy = Speakeasy(HumanList, 10000, my_bar_stock, my_bar_prices, True)
 
 print "People in the speakeasy: " + str(mySpeakeasy.people)
+print "Initial stock: " + str(my_bar_stock)
 
 print Charles.buyDrink(Charles.money, my_bar_stock, Charles.desparation, my_bar_prices, True, Charles.thirst)
 
-print "Binge time! Down that Whiskey!"
+print "Binge time! Down that special drank!"
 
-for x in range(3):
+
+    
+while Charles.thirst >= 1:
     print Charles.buyDrink(Charles.money, my_bar_stock, Charles.desparation, my_bar_prices, True, Charles.thirst)
     
-print my_bar_stock
+print "Final stock: " + str(my_bar_stock)
+"""
+
+
+
+def storeHuman(person):
+    if person.customer == True:
+        CustomerList.append(person.name)
+        return CustomerList
+    elif person.employee == True:
+        EmployeeList.append(person.name)
+        return EmployeeList
+    else:
+        print "This isn't a person."
+        return
+    return HumanList
+    print HumanList
+    print EmployeeList
+    print CustomerList
+
+James = Human("James", 100, 10, 0, False, True, 5, 100)
+
+storeHuman(James)
+
